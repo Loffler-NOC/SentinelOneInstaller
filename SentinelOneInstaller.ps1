a80bb82d66cf2d934f44f28a195d8f2e7a1dd79b
Loffler-NOC <150292433+Loffler-NOC@users.noreply.github.com>
2024-01-17 09:58:58 -0600

    Changing all exit codes to 1
    Changing all exit codes to 1. Datto RMM cannot meaningfully parse different exit codes, and reason for error will be written to the log.

--- SentinelOneInstaller.ps1
+++ SentinelOneInstaller.ps1
@@ -13,7 +13,7 @@ catch {
 
 if ($installed) {
     Write-Output "SentinelOne is already installed."
-    exit 2
+    exit 1
 } 
 else {
     Write-Host "SentinelOne is not installed, proceeding to next check"
@@ -25,30 +25,30 @@ else {
 
 if ($env:UDF_11 -eq 1) {
     Write-Output "SentinelOne deployment is disabled at the device level, exiting script."
-    exit 3
+    exit 1
 } 
 elseif ($env:UDF_11 -eq 0) {
     Write-Output "SentinelOne deployment is enabled at the device level, proceeding to next check."
 }
 elseif (!$env:UDF_11) {
     Write-Output "UDF11 DisableS1AutoDeploy is blank, value is reporting as: "$env:UDF_11" , valid values are 0 or 1, exiting script with error"
-    exit 4
+    exit 1
 }
 else {
     Write-Output "UDF11 DisableS1AutoDeploy has an unexpected value of $env:UDF_11 , valid values are 0 or 1, exiting script with error"
-    exit 5
+    exit 1
 }
 
 if ($env:SentinelOneDeployEnabled -eq 0) {
     Write-Output "$programName deployment is disabled at the site level, exiting script."
-    exit 6
+    exit 1
 } 
 elseif ($env:SentinelOneDeployEnabled -eq 1) {
     Write-Output "$programName deployment is enabled at the device level, proceeding to next check."
 }
 else {
     Write-Output "Site variable SentinelOneDeployEnabled has an unexpected value of $env:SentinelOneDeployEnabled , valid values are 0 or 1, exiting script with error"
-    exit 7
+    exit 1
 }
 
 #Check that site token is filled out
@@ -57,11 +57,11 @@ else {
 
 if (!$env:S1SiteToken) {
     Write-Output "S1 site token not found, please check site variables for S1SiteToken and make sure it exists and is filled out"
-    exit 8
+    exit 1
 }
 elseif ($env:S1SiteToken -eq 0) {
     Write-Output "S1 site token still has the default value of 0, please check site variables for S1SiteToken and make sure it exists and is filled out"
-    exit 9
+    exit 1
 }
 else {
     Write-Output "S1 site token is $env:S1SiteToken, proceeding to next step."
@@ -74,7 +74,7 @@ try {
 catch {
     Write-Output "S1 was not able to be downloaded. Please check that the device is able to reach https://update.itsupport247.net/SentinelOne/SentinelOne_windows.exe . Full error message:"
     Write-Output $_
-    exit 10
+    exit 1
 }
 
 #Run the S1 installer
@@ -84,7 +84,7 @@ try {
 catch {
     Write-Output "S1 agent was not able to install successfully. Full error message:"
     Write-Output $_
-    exit 11
+    exit 1
 }
 
 #Clean up the S1 installer
@@ -94,7 +94,7 @@ try {
 catch {
     Write-Output "Could not clean up installer. Please check C:\Software\SentinelAgent.exe and see if it was removed. Full error message:"
     Write-Output $_
-    exit 12
+    exit 1
 }
 
 #Check that S1 was installed
@@ -104,7 +104,7 @@ try {
 catch {
     Write-Output "Unable to read HKLM\SYSTEM\CurrentControlSet\Services\SentinelAgent\config\ . Cannot determine if S1 was installed successfully. Full error message:"
     Write-Output $_
-    exit 13
+    exit 1
 }
 
 if ($S1installed) {
@@ -114,5 +114,5 @@ if ($S1installed) {
 } 
 else {
     Write-Host "SentinelOne is not installed."
-    exit 14
+    exit 1
 }
